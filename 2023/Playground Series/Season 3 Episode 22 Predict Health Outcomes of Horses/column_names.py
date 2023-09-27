78773789892d8bb5eb558125489a8f11ec178651
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-09-27 12:40:26 +0200

    feat: add column names

---
+++ 2023/Playground Series/Season 3 Episode 22 Predict Health Outcomes of Horses/column_names.py
@@ -0,0 +1,32 @@
+quanti_var = [
+    'hospital_number',
+    'rectal_temp',
+    'pulse',
+    'respiratory_rate',
+    'nasogastric_reflux_ph',
+    'rectal_exam_feces',
+    'abdomen',
+    'packed_cell_volume',
+    'total_protein',
+    'abdomo_appearance',
+    'abdomo_protein',
+    'surgical_lesion',
+    'lesion_1',
+    'lesion_2',
+    'lesion_3',
+    'cp_data',
+]
+
+quali_var = [
+    'surgery',
+    'age',
+    'temp_of_extremities',
+    'peripheral_pulse',
+    'mucous_membrane',
+    'capillary_refill_time',
+    'pain',
+    'peristalsis',
+    'abdominal_distention',
+    'nasogastric_tube',
+    'nasogastric_reflux',
+]
