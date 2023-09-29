ad4f142f65c111f6086c4e621ddb6ce9207b0c86
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-09-29 12:44:23 +0200

    refactor: move functions to prediction.py

---
+++ 2023/Playground Series/Season 3 Episode 22 Predict Health Outcomes of Horses/prediction.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+# from catboost import CatBoostClassifier
+# from imblearn.over_sampling import SMOTEN, SMOTENC
+# from keras import layers
+from lightgbm import LGBMClassifier
+from sklearn.base import BaseEstimator
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.dummy import DummyClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+
+
+# from tensorflow import keras
+# from xgboost import XGBClassifier
+
+def create_models(seed: int) -> dict[str, BaseEstimator]:
+    return {
+        "DummyClassifier_Uniform": DummyClassifier(strategy="uniform", random_state=seed),
+        "DummyClassifier_MostFrequent": DummyClassifier(
+            strategy="most_frequent", random_state=seed
+        ),
+        "KNeighborsClassifier5": KNeighborsClassifier(),
+        "LinearSVC": LinearSVC(random_state=seed),
+        "LogisticRegression": LogisticRegression(random_state=seed),
+        "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
+        "RandomForestClassifier": RandomForestClassifier(random_state=seed),
+        # "XGBClassifier": XGBClassifier(random_state=seed),
+        # "CatBoostClassifier": CatBoostClassifier(random_state=seed, verbose=False),
+        "LGBMClassifier": LGBMClassifier(random_state=seed),
+    }
+
+
+def evaluate_models(
+        models: dict[str, BaseEstimator],
+        prefix: str,
+        X_train: pd.DataFrame | np.ndarray,
+        y_train: pd.DataFrame | np.ndarray
+):
+    """Evalue tous les modèles dans `models` et sauvegarde les résultats avec un préfixe `prefix`
+    (utile pour distinguer les différentes stratégies de pré-traitement des données)."""
+    results = []
+
+    for model_name, model in models.items():
+        name = f"{prefix}/{model_name}"
+        print(name)
+
+        scores = cross_val_score(
+            model,
+            X_train,
+            y_train,
+            cv=10,
+            scoring="f1_micro",
+        )
+
+        scores_mean = scores.mean()
+        scores_std = scores.std()
+
+        results.append(
+            [
+                name,
+                scores_mean,
+                scores_std,
+            ]
+        )
+
+    print(sorted(results, key=lambda x: x[1], reverse=True))
+
+    return results
