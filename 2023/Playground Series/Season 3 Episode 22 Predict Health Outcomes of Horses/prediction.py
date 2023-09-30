6cc0c79cdbf6cf67dc6889704d193a518e668323
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-09-30 21:43:16 +0200

    refactor: create `make_prediction` function

--- 2023/Playground Series/Season 3 Episode 22 Predict Health Outcomes of Horses/prediction.py
+++ 2023/Playground Series/Season 3 Episode 22 Predict Health Outcomes of Horses/prediction.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-# from catboost import CatBoostClassifier
-# from imblearn.over_sampling import SMOTEN, SMOTENC
-# from keras import layers
+from catboost import CatBoostClassifier
+from column_names import target
 from lightgbm import LGBMClassifier
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.compose import ColumnTransformer
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -12,14 +12,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
+from xgboost import XGBClassifier
 
-
-# from tensorflow import keras
-# from xgboost import XGBClassifier
 
 def create_models(seed: int) -> dict[str, BaseEstimator]:
+    """todo"""
     return {
-        "DummyClassifier_Uniform": DummyClassifier(strategy="uniform", random_state=seed),
+        "DummyClassifier_Uniform": DummyClassifier(
+            strategy="uniform", random_state=seed
+        ),
         "DummyClassifier_MostFrequent": DummyClassifier(
             strategy="most_frequent", random_state=seed
         ),
@@ -28,18 +29,18 @@ def create_models(seed: int) -> dict[str, BaseEstimator]:
         "LogisticRegression": LogisticRegression(random_state=seed),
         "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
         "RandomForestClassifier": RandomForestClassifier(random_state=seed),
-        # "XGBClassifier": XGBClassifier(random_state=seed),
-        # "CatBoostClassifier": CatBoostClassifier(random_state=seed, verbose=False),
+        "XGBClassifier": XGBClassifier(random_state=seed),
+        "CatBoostClassifier": CatBoostClassifier(random_state=seed, verbose=False),
         "LGBMClassifier": LGBMClassifier(random_state=seed),
     }
 
 
 def evaluate_models(
-        models: dict[str, BaseEstimator],
-        prefix: str,
-        X_train: pd.DataFrame | np.ndarray,
-        y_train: pd.DataFrame | np.ndarray
-):
+    models: dict[str, BaseEstimator],
+    prefix: str,
+    X_train: pd.DataFrame | np.ndarray,
+    y_train: pd.DataFrame | np.ndarray,
+) -> list[list[str]]:
     """Evalue tous les modèles dans `models` et sauvegarde les résultats avec un préfixe `prefix`
     (utile pour distinguer les différentes stratégies de pré-traitement des données)."""
     results = []
@@ -70,3 +71,24 @@ def evaluate_models(
     print(sorted(results, key=lambda x: x[1], reverse=True))
 
     return results
+
+
+def make_prediction(
+    model: BaseEstimator,  # todo: maybe type isn't 100% accurate
+    X_train: pd.DataFrame | np.ndarray,
+    y_train: pd.DataFrame | np.ndarray,
+    X_kaggle: pd.DataFrame | np.ndarray,
+    X_preprocessor: ColumnTransformer,  # todo: maybe type isn't 100% accurate
+    y_preprocessor: TransformerMixin,  # todo: maybe type isn't 100% accurate
+) -> pd.DataFrame:
+    """todo"""
+    model.fit(X_train, y_train)
+
+    X_kaggle_processed = pd.DataFrame(
+        X_preprocessor.transform(X_kaggle),
+        columns=X_preprocessor.get_feature_names_out(),
+    )
+
+    y_pred = y_preprocessor.inverse_transform(model.predict(X_kaggle_processed))
+
+    return pd.DataFrame(y_pred, index=X_kaggle.index, columns=[target])
