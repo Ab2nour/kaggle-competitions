b3f72fabb19ac97dc7b6be4be5d703d2aebd27fc
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-10-08 19:49:06 +0200

    refactor: create models.py file for model list

--- 2023/Playground Series/Season 3/Episode 23 Software Defects Dataset/prediction.py
+++ 2023/Playground Series/Season 3/Episode 23 Software Defects Dataset/prediction.py
@@ -24,26 +24,6 @@ from sklearn.svm import LinearSVC
 from xgboost import XGBClassifier
 
 
-def create_models(seed: int) -> dict[str, BaseEstimator]:
-    """todo"""
-    return {
-        "DummyClassifier_Uniform": DummyClassifier(
-            strategy="uniform", random_state=seed
-        ),
-        "DummyClassifier_MostFrequent": DummyClassifier(
-            strategy="most_frequent", random_state=seed
-        ),
-        "KNeighborsClassifier5": KNeighborsClassifier(),
-        "LinearSVC": LinearSVC(random_state=seed),
-        "LogisticRegression": LogisticRegression(random_state=seed),
-        "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
-        "RandomForestClassifier": RandomForestClassifier(random_state=seed),
-        "XGBClassifier": XGBClassifier(random_state=seed),
-        "CatBoostClassifier": CatBoostClassifier(random_state=seed, verbose=False),
-        "LGBMClassifier": LGBMClassifier(random_state=seed),
-    }
-
-
 def evaluate_models(
     models: dict[str, BaseEstimator],
     prefix: str,
