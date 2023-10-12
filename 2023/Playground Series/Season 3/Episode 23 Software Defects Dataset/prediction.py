6148b1c7f44f7d117db7c07c46591969f1152607
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-10-12 11:47:58 +0200

    refactor: remove pipelines from `prediction.py`

--- 2023/Playground Series/Season 3/Episode 23 Software Defects Dataset/prediction.py
+++ 2023/Playground Series/Season 3/Episode 23 Software Defects Dataset/prediction.py
@@ -3,16 +3,7 @@ import pandas as pd
 from column_names import quali_var_binary, quali_var_for_ohe, quanti_var, target
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.model_selection import cross_val_score
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import (
-    FunctionTransformer,
-    LabelEncoder,
-    OneHotEncoder,
-    OrdinalEncoder,
-    StandardScaler,
-)
 
 
 def evaluate_models(
@@ -70,81 +61,3 @@ def make_prediction(
     y_pred = y_preprocessor.inverse_transform(raw_predictions)
 
     return pd.DataFrame(y_pred, index=X_kaggle.index, columns=[target])
-
-
-def add_original_data(
-    X: np.ndarray, y: np.ndarray, X_original: np.ndarray, y_original: np.ndarray
-) -> tuple[np.ndarray, np.ndarray]:
-    X_with_original = np.vstack((X, X_original))
-    y_with_original = np.hstack((y, y_original))
-
-    return X_with_original, y_with_original
-
-
-def add_features(df: pd.DataFrame) -> pd.DataFrame:
-    return df
-
-
-def create_x_pipeline():
-    """todo"""
-    add_features_transformer = FunctionTransformer(add_features)
-
-    quanti_processor = Pipeline(
-        steps=[
-            ("imputer", SimpleImputer()),
-            ("scaler", StandardScaler()),
-        ]
-    )
-
-    quali_ohe_processor = Pipeline(
-        steps=[
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            (
-                "ohe_encoder",
-                OneHotEncoder(
-                    handle_unknown="infrequent_if_exist", sparse_output=False
-                ),
-            ),
-        ]
-    )
-
-    quali_binary_processor = Pipeline(
-        steps=[
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            (
-                "encoder",
-                OrdinalEncoder(
-                    handle_unknown="use_encoded_value", unknown_value=np.nan
-                ),
-            ),
-        ]
-    )
-
-    column_transformer = ColumnTransformer(
-        remainder="passthrough",
-        transformers=[
-            (
-                "quali_ohe",
-                quali_ohe_processor,
-                quali_var_for_ohe,
-            ),
-            ("quali_non_ohe", quali_binary_processor, quali_var_binary),
-            ("quanti_processor", quanti_processor, quanti_var),
-        ],
-    )
-
-    preprocessor = Pipeline(
-        steps=[
-            ("add_features", add_features_transformer),
-            ("column_transformer", column_transformer),
-        ]
-    )
-
-    return preprocessor
-
-
-def create_y_pipeline():
-    """todo"""
-    preprocessor = LabelEncoder()
-
-    return preprocessor
