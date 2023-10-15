3e83fb70640c591e523537244846ec6a8ebdfe96
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-10-15 21:30:23 +0200

    feat(wip): add grid_search code

---
+++ 2023/Playground Series/Season 3/Episode 23 Software Defects Dataset/grid_search.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import xgboost
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+
+def gs(x_train, y_train, model, model_params, cv, scoring: str = "roc_auc"):
+    model_name = model.__class__.__name__
+
+    clf = GridSearchCV(
+        estimator=model,
+        param_grid=model_params,
+        scoring=scoring,
+        n_jobs=-1,
+        verbose=1,
+        cv=cv,
+    )
+
+    clf.fit(x_train, y_train)
+
+    results = pd.DataFrame(clf.cv_results_)
+
+    results.to_csv(
+        f"data/hyper-parameter-tuning/grid-search/{model_name}.csv", index=False
+    )
+
+
+def gs_xgboost(x_train, y_train):
+    model = xgboost.XGBClassifier(n_jobs=-1, random_state=0)
+
+    model_params = {
+        "max_depth": [3, 6, 10],
+        "learning_rate": [0.01, 0.05, 0.1],
+        "n_estimators": [100, 500, 1000],
+        "colsample_bytree": [0.3, 0.7],
+    }
+
+    kfold = StratifiedKFold(shuffle=True, random_state=0)
+
+    gs(x_train, y_train, model, model_params, kfold)
