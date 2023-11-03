56a6e56c9df911c8f424508294446071776d361d
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-11-03 20:24:17 +0100

    feat: add s3 ep24

---
+++ 2023/Playground Series/Season 3/Episode 24 Smoker Status using Bio-Signals/column_names.py
@@ -0,0 +1,35 @@
+target = "defects"
+id_col = "id"
+
+quanti_var = [
+    "loc",
+    "v(g)",
+    "ev(g)",
+    "iv(g)",
+    "n",
+    "v",
+    "l",
+    "d",
+    "i",
+    "e",
+    "b",
+    "t",
+    "lOCode",
+    "lOComment",
+    "lOBlank",
+    "locCodeAndComment",
+    "uniq_Op",
+    "uniq_Opnd",
+    "total_Op",
+    "total_Opnd",
+    "branchCount",
+]
+
+quali_var = [
+]
+
+quali_var_binary = [
+]
+
+quali_var_for_ohe = [
+]
---
+++ 2023/Playground Series/Season 3/Episode 24 Smoker Status using Bio-Signals/models.py
@@ -0,0 +1,37 @@
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
+from sklearn.base import BaseEstimator
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.dummy import DummyClassifier
+from sklearn.ensemble import (
+    ExtraTreesClassifier,
+    HistGradientBoostingClassifier,
+    RandomForestClassifier,
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier
+
+
+def create_models(seed: int) -> dict[str, BaseEstimator]:
+    """todo"""
+    return {
+        "DummyClassifier_Uniform": DummyClassifier(
+            strategy="uniform", random_state=seed
+        ),
+        "DummyClassifier_MostFrequent": DummyClassifier(
+            strategy="most_frequent", random_state=seed
+        ),
+        # "KNeighborsClassifier5": KNeighborsClassifier(),
+        # "LinearSVC": LinearSVC(random_state=seed),
+        "LogisticRegression": LogisticRegression(random_state=seed),
+        "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
+        "RandomForestClassifier": RandomForestClassifier(random_state=seed),
+        "ExtraTreesClassifier": ExtraTreesClassifier(random_state=seed),
+        "HistGradientBoostingClassifier": HistGradientBoostingClassifier(
+            random_state=seed
+        ),
+        "XGBClassifier": XGBClassifier(n_jobs=-1, random_state=seed),
+        "CatBoostClassifier": CatBoostClassifier(random_state=seed, verbose=False),
+        "LGBMClassifier": LGBMClassifier(random_state=seed),
+    }
