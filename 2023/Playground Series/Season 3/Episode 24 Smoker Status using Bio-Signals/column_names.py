6bcfebd6477dfd295b50b0e18e6d7f2549253d79
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-11-13 22:34:16 +0100

    feat: start of s3ep24

--- 2023/Playground Series/Season 3/Episode 24 Smoker Status using Bio-Signals/column_names.py
+++ 2023/Playground Series/Season 3/Episode 24 Smoker Status using Bio-Signals/column_names.py
@@ -1,35 +1,40 @@
-target = "defects"
+target = "smoking"
 id_col = "id"
 
 quanti_var = [
-    "loc",
-    "v(g)",
-    "ev(g)",
-    "iv(g)",
-    "n",
-    "v",
-    "l",
-    "d",
-    "i",
-    "e",
-    "b",
-    "t",
-    "lOCode",
-    "lOComment",
-    "lOBlank",
-    "locCodeAndComment",
-    "uniq_Op",
-    "uniq_Opnd",
-    "total_Op",
-    "total_Opnd",
-    "branchCount",
+    "age",
+    "height(cm)",
+    "weight(kg)",
+    "waist(cm)",
+    "eyesight(left)",
+    "eyesight(right)",
+    "systolic",
+    "relaxation",
+    "fasting blood sugar",
+    "Cholesterol",
+    "triglyceride",
+    "HDL",
+    "LDL",
+    "hemoglobin",
+    "serum creatinine",
+    "AST",
+    "ALT",
+    "Gtp",
 ]
 
 quali_var = [
+    "Urine protein",
+    "dental caries",
+    "hearing(left)",
+    "hearing(right)",
 ]
 
 quali_var_binary = [
+    "dental caries",
+    "hearing(left)",
+    "hearing(right)",
 ]
 
 quali_var_for_ohe = [
+    "Urine protein",
 ]
